cc999c461ba0ec1d751973cba0b4bf504be7b72b
noorall <863485501@qq.com>
2024-06-14 14:25:22 +0800

    test: add UT for correctness interpretation of JobGraph

--- flink-runtime/src/test/java/org/apache/flink/streaming/api/graph/AdaptiveJobGraphManagerTest.java
+++ flink-runtime/src/test/java/org/apache/flink/streaming/api/graph/AdaptiveJobGraphManagerTest.java
@@ -300,6 +300,47 @@ class AdaptiveJobGraphManagerTest {
     }
 
     @Test
+    void testTheCorrectnessOfJobGraph(){
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStream<Tuple2<String, String>> input =
+                env.fromData("a", "b", "c", "d", "e", "f")
+                        .map(
+                                new MapFunction<String, Tuple2<String, String>>() {
+
+                                    @Override
+                                    public Tuple2<String, String> map(String value) {
+                                        return new Tuple2<>(value, value);
+                                    }
+                                });
+
+        DataStream<Tuple2<String, String>> result =
+                input.keyBy(0)
+                        .map(
+                                new MapFunction<Tuple2<String, String>, Tuple2<String, String>>() {
+
+                                    @Override
+                                    public Tuple2<String, String> map(
+                                            Tuple2<String, String> value) {
+                                        return value;
+                                    }
+                                });
+
+        result.addSink(
+                new SinkFunction<Tuple2<String, String>>() {
+                    @Override
+                    public void invoke(Tuple2<String, String> value) {}
+                });
+        StreamGraph streamGraph = env.getStreamGraph();
+        JobGraph jobGraph1=generateJobGraphInLazilyMode(streamGraph);
+        JobGraph jobGraph2=StreamingJobGraphGenerator.createJobGraph(streamGraph);
+        assertThat(isJobGraphEquivalent(jobGraph1,jobGraph2)).isEqualTo(true);
+    }
+
+
+
+    @Test
     void testUpdateStreamGraph() {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // fromElements -> Map -> Print
@@ -3066,5 +3107,42 @@ class AdaptiveJobGraphManagerTest {
             serializationExecutor.shutdown();
         }
         return adaptiveJobGraphGenerator.getJobGraph();
+    }
+
+    private static boolean isJobGraphEquivalent(JobGraph jobGraph1, JobGraph jobGraph2){
+        assertThat(jobGraph1.getJobConfiguration()).isEqualTo(jobGraph2.getJobConfiguration());
+        assertThat(jobGraph1.getJobType()).isEqualTo(jobGraph2.getJobType());
+        assertThat(jobGraph1.isDynamic()).isEqualTo(jobGraph2.isDynamic());
+        assertThat(jobGraph1.isApproximateLocalRecoveryEnabled()).isEqualTo(jobGraph2.isApproximateLocalRecoveryEnabled());
+        assertThat(jobGraph1.getSerializedExecutionConfig()).isEqualTo(jobGraph2.getSerializedExecutionConfig());
+        assertThat(jobGraph1.getCheckpointingSettings()).isEqualTo(jobGraph2.getCheckpointingSettings());
+        assertThat(jobGraph1.getSavepointRestoreSettings()).isEqualTo(jobGraph2.getSavepointRestoreSettings());
+        assertThat(jobGraph1.getUserJars()).isEqualTo(jobGraph2.getUserJars());
+        assertThat(jobGraph1.getUserArtifacts()).isEqualTo(jobGraph2.getUserArtifacts());
+        assertThat(jobGraph1.getUserJarBlobKeys()).isEqualTo(jobGraph2.getUserJarBlobKeys());
+        assertThat(jobGraph1.getClasspaths()).isEqualTo(jobGraph2.getClasspaths());
+        assertThat(jobGraph1.getJobStatusHooks()).isEqualTo(jobGraph2.getJobStatusHooks());
+
+        List<JobVertex> vertices1 = jobGraph1.getVerticesSortedTopologicallyFromSources();
+        List<JobVertex> vertices2 = jobGraph2.getVerticesSortedTopologicallyFromSources();
+        assertThat(vertices1.size()).isEqualTo(vertices2.size());
+        for(int i=1;i<vertices1.size();i++){
+            JobVertex vertex1 = vertices1.get(i);
+            JobVertex vertex2 = vertices2.get(i);
+            assertThat(vertex1.getID()).isEqualTo(vertex2.getID());
+            assertThat(vertex1.getInputs().size()).isEqualTo(vertex2.getInputs().size());
+            assertThat(vertex1.getParallelism()).isEqualTo(vertex2.getParallelism());
+            assertThat(vertex1.getMaxParallelism()).isEqualTo(vertex2.getMaxParallelism());
+            assertThat(vertex1.getMinResources()).isEqualTo(vertex2.getMinResources());
+            assertThat(vertex1.getPreferredResources()).isEqualTo(vertex2.getPreferredResources());
+            assertThat(vertex1.getInvokableClassName()).isEqualTo(vertex2.getInvokableClassName());
+            assertThat(vertex1.getName()).isEqualTo(vertex2.getName());
+            assertThat(vertex1.getOperatorName()).isEqualTo(vertex2.getOperatorName());
+            assertThat(vertex1.isSupportsConcurrentExecutionAttempts()).isEqualTo(vertex2.isSupportsConcurrentExecutionAttempts());
+            assertThat(vertex1.isAnyOutputBlocking()).isEqualTo(vertex2.isAnyOutputBlocking());
+            assertThat(vertex1.isParallelismConfigured()).isEqualTo(vertex2.isParallelismConfigured());
+        }
+
+        return true;
     }
 }
