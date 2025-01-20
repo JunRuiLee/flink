24373e785c259b91ae61935fbb1a5d721eca7211
JunRuiLee <jrlee.ljr@gmail.com>
2025-01-20 13:30:49 +0800

    Revert "[FLINK-36608][table-runtime] Add shouldReorderInputs method to AdaptiveJoin."
    This reverts commit 627395acf46b8b38ee2c8db2cb6ba31849db9f27.

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/adaptive/AdaptiveJoinOperatorGenerator.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/adaptive/AdaptiveJoinOperatorGenerator.java
@@ -155,15 +155,4 @@ public class AdaptiveJoinOperatorGenerator implements AdaptiveJoin {
         this.isBroadcastJoin = canBroadcast;
         this.leftIsBuild = leftIsBuild;
     }
-
-    @Override
-    public boolean shouldReorderInputs() {
-        // Sort merge join requires the left side to be read first if the broadcast threshold is not
-        // met.
-        if (!isBroadcastJoin && originalJoin == OperatorType.SortMergeJoin) {
-            return false;
-        }
-
-        return !leftIsBuild;
-    }
 }
